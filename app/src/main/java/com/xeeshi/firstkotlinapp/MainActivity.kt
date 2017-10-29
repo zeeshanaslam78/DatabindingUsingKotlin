19d930bfdb92ec50c725d249a26e9672e4225438
Zeeshan Aslam <zeeshan@wadic.net>
2017-10-29 20:02:08 +0100

    binding view

--- app/src/main/java/com/xeeshi/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/xeeshi/firstkotlinapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.xeeshi.firstkotlinapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.xeeshi.firstkotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //var a: String?
 
+    lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main)
+
+        binding.repositoryName.text = "Android Medium Article"
     }
 }
