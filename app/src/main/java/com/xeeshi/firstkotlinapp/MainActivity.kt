f1387ca2ff5f0a69b535c130c8caf3c4bde8c398
Zeeshan Aslam <zeeshan@wadic.net>
2017-10-29 19:01:53 +0100

    Initial commit

---
+++ app/src/main/java/com/xeeshi/firstkotlinapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xeeshi.firstkotlinapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    //var a: String?
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
